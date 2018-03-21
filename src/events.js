0eb824325b236b2a777ed14c77a4ed2afc756580
Chris Thomas <chris@admt.im>
2018-03-21 14:40:25 -0700

    React Calendar

--- src/events.js
+++ src/events.js
@@ -1,12 +1,30 @@
 const events = [
     {
         id: 1,
-        name: 'Curse of Strahd',
+        name: 'Curse of Strahd with Draco',
         date: {
-            day: 12,
-            month: 'Jun'
+            day: 25,
+            month: 'Mar'
         },
-        watermark:'DnD'
+        watermark: 'DnD'
+    },
+    {
+        id: 2,
+        name: 'Tomb of Annihilation with Mira',
+        date: {
+            day: 31,
+            month: 'Mar'
+        },
+        watermark: 'DnD'
+    },
+    {
+        id: 3,
+        name: 'Princes of the Apocalypse with Faore',
+        date: {
+            day: 8,
+            month: 'Apr'
+        },
+        watermark: 'DnD'
     }
 ];
 
