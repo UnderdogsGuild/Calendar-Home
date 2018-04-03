8b5562e9db093475a4b1449c67be9a4f97130596
Chris Thomas <chris@admt.im>
2018-04-02 17:34:35 -0700

    Update events.js

--- src/events.js
+++ src/events.js
@@ -1,23 +1,5 @@
 const events = [
     {
-        id: 1,
-        name: 'Curse of Strahd with Draco',
-        date: {
-            day: 25,
-            month: 'Mar'
-        },
-        watermark: 'DnD'
-    },
-    {
-        id: 2,
-        name: 'Tomb of Annihilation with Mira',
-        date: {
-            day: 31,
-            month: 'Mar'
-        },
-        watermark: 'DnD'
-    },
-    {
         id: 3,
         name: 'Princes of the Apocalypse with Faore',
         date: {
@@ -25,7 +7,25 @@ const events = [
             month: 'Apr'
         },
         watermark: 'DnD'
-    }
+    },
+    {
+        id: 4,
+        name: 'Curse of Strahd with Draco',
+        date: {
+            day: 15,
+            month: 'Apr'
+        },
+        watermark: 'DnD'
+    },
+    {
+        id: 5,
+        name: 'Tomb of Annihilation with Mira',
+        date: {
+            day: 22,
+            month: 'Apr'
+        },
+        watermark: 'DnD'
+    },
 ];
 
 export default events;
